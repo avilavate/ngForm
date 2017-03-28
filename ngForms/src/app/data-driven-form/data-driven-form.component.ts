4ecc176faaee0160925ffdd2b729b87b4f258483
avilavate <avilavate123@gmail.com>
2017-03-28 10:44:50 +0530

    Added synchronous validator in data-driven form.

--- ngForms/src/app/data-driven-form/data-driven-form.component.ts
+++ ngForms/src/app/data-driven-form/data-driven-form.component.ts
@@ -13,15 +13,21 @@ export class DataDrivenFormComponent implements OnInit {
     this.myForm = new FormGroup(
       {
         'username': new FormControl('Max', Validators.required),
-        'password': new FormControl('', Validators.required),
+        'password': new FormControl('', [Validators.required, this.myValidator]),
         'email': new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)]),
       }
     )
   }
+  myValidator(password: FormControl): { [key: string]: boolean } {
+    if (<string>password.value === 'Avil') {
+      return { 'password': false };
+    }
+  }
+
   submitForm() {
     console.log(this.myForm);
   }
-  addHobby(){
+  addHobby() {
 
   }
   ngOnInit() {
